22133ee6fcfb30c3ca2a75a0ae4386817d374494
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2016-01-08 18:01:18 +0800

    first commit

---
+++ py/spider.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Geoffrey Zhao'
+
+import requests
+from bs4 import BeautifulSoup
+
+# html = requests.get('http://www.travelzen.com/milestone.html')
+# html.encoding = 'utf-8'
+# with open('../doc/info.txt', 'w', encoding='utf-8') as f:
+#     f.write(html.text)
+
+# soup = BeautifulSoup(html.text, 'lxml')
+# print(soup.title.string)
+print("你好")
